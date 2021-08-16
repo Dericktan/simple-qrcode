1ba19f82e0a455ebd411224911d7ee8954e52526
Derick <derick.tan988@gmail.com>
2021-08-17 02:22:51 +0700

    Add support for rounded square eye

--- src/Generator.php
+++ src/Generator.php
@@ -12,6 +12,7 @@ use BaconQrCode\Renderer\Eye\EyeInterface;
 use BaconQrCode\Renderer\Eye\ModuleEye;
 use BaconQrCode\Renderer\Eye\SimpleCircleEye;
 use BaconQrCode\Renderer\Eye\SquareEye;
+use BaconQrCode\Renderer\Eye\RoundedSquareEye; 
 use BaconQrCode\Renderer\Image\EpsImageBackEnd;
 use BaconQrCode\Renderer\Image\ImageBackEndInterface;
 use BaconQrCode\Renderer\Image\ImagickImageBackEnd;
@@ -502,6 +503,10 @@ class Generator
 
         if ($this->eyeStyle === 'circle') {
             return SimpleCircleEye::instance();
+        }
+
+        if ($this->eyeStyle === 'rounded-square') {
+            return RoundedSquareEye::instance();
         }
 
         return new ModuleEye($this->getModule());
